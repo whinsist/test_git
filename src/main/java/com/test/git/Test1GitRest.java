7530d8cc64f246356e8ec9bb529914dcc35a9548
wuhong <wuhong@cloud-star.com.cn>
2019-05-13 17:52:13 +0800

    test reset 第2次

--- src/main/java/com/test/git/Test1GitRest.java
+++ src/main/java/com/test/git/Test1GitRest.java
@@ -17,5 +17,7 @@ package com.test.git;
 public class Test1GitRest {
     public static void main(String[] args) {
         System.out.println("11111111111111111");
+        System.out.println("2222222222222222");
+
     }
 }
