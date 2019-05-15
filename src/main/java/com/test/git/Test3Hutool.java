073edd33f2044612081712512c397199b82729f8
wuhong_home <444089024@qq.com>
2019-05-15 23:58:31 +0800

    io使用缓冲

--- src/main/java/com/test/git/Test3Hutool.java
+++ src/main/java/com/test/git/Test3Hutool.java
@@ -6,9 +6,9 @@ import java.io.FileOutputStream;
 
 
 //https://blog.csdn.net/moshowgame/article/details/80087954
+//https://blog.csdn.net/youanyyou/article/details/79602592
 public class Test3Hutool {
     public static void main(String[] args) {
-
 
     }
 
