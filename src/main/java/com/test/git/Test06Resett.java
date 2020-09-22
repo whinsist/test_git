b830c8811cb02d0e10dcffc68b8a6631af9a9c6e
wuhong <wuhong@cloud-star.com.cn>
2020-09-22 12:57:43 +0800

    test reset

---
+++ src/main/java/com/test/git/Test06Resett.java
@@ -0,0 +1,11 @@
+package com.test.git;
+
+/**
+ * @author Hong.Wu
+ * @date: 12:33 2020/09/22
+ */
+public class Test06Resett {
+    public static void main(String[] args) {
+        System.out.println("testsssss");
+    }
+}
