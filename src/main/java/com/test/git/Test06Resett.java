a22cfdb9363f8d82be8e1ddb61b767dbdb896d57
wuhong <wuhong@cloud-star.com.cn>
2020-09-23 10:53:30 +0800

    重命名222

--- src/main/java/com/test/git/Test06Resett.java
+++ src/main/java/com/test/git/Test06Resett.java
@@ -7,6 +7,6 @@ package com.test.git;
 public class Test06Resett {
     public static void main(String[] args) {
         System.out.println("testsssss");
-        System.out.println("强制与服务器一致：git reset --hard origin/test");
+        System.out.println("强制与服务器一致：git reset --hard origin/test111");
     }
 }
